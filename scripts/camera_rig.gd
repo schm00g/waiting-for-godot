7205b7d14814fd63e14ddb2f05381175479c1c54
schm00g <hk8igureu@relay.firefox.com>
2026-09-09 21:00:22 +0100

    animation tree

--- scripts/camera_rig.gd
+++ scripts/camera_rig.gd
@@ -4,6 +4,8 @@ extends SpringArm3D
 @export var turn_rate:= 1
 var mouse_input: Vector2 = Vector2()
 var mouse_sensitivity:= 0.07
+@onready var player: Node3D = get_parent()
+var camer_rig_height: float = position.y
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,12 +15,13 @@ func _ready() -> void:
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var look_input:= Input.get_vector("view_left", "view_right", "view_up", "view_down")
+	var look_input:= Input.get_vector("view_right", "view_left", "view_down", "view_up")
 	look_input = turn_rate * look_input * delta
 	look_input += mouse_input 
 	mouse_input = Vector2()
-	rotation_degrees.x += look_input.x
-	rotation_degrees.y += look_input.y
+	
+	rotation_degrees.x += look_input.y
+	rotation_degrees.y += look_input.x
 	rotation_degrees.x = clampf(rotation_degrees.x, -70, 50)
 
 func _input(event: InputEvent) -> void:
@@ -28,3 +31,6 @@ func _input(event: InputEvent) -> void:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta: float) -> void:
+	position = player.position + Vector3(0, camer_rig_height, 0)
--- scripts/character_body_3d.gd
+++ scripts/character_body_3d.gd
@@ -1,8 +1,11 @@
 extends CharacterBody3D
+@onready var anim_player: AnimationPlayer = $Mesh/AnimationPlayer
+@onready var anim_tree: AnimationTree = $AnimationTree
 
 ## Determines how fast the player moves
 @export var speed := 5.0
 const JUMP_VELOCITY = 4.5
+@onready var camera: Node3D = $CameraRig/Camera3D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,7 +19,8 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (camera.global_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = Vector3(direction.x, 0, direction.z).normalized() * input_dir.length()
 	if direction:
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
@@ -25,3 +29,26 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
+	turn_to(direction)
+	
+	var current_speed:= velocity.length()
+	const RUN_SPEED := 3;
+	const BLEND_SPEED := 0.2;
+	
+	if current_speed > RUN_SPEED:
+		anim_tree.set("parameters/movement/transition_request", "run")
+		var lean := direction.dot(global_basis.x)
+		anim_tree.set("paramaters/run_lean/add_amount", lean)
+		# https://www.youtube.com/watch?v=L4EYYogZlBA 18:49
+	elif current_speed > 0.1:
+		anim_tree.set("parameters/movement/transition_request", "walk")
+		var walk_speed := lerpf(0.5, 1.75, current_speed / RUN_SPEED)
+		anim_tree.set("parameters/walk_speed/scale", walk_speed)
+	else:
+		anim_tree.set("parameters/movement/transition_request", "idle")
+	
+func turn_to(direction: Vector3) -> void:
+	if direction.length() > 0:
+		var yaw:= atan2(-direction.x, -direction.z)
+		yaw = lerp_angle(rotation.y, yaw, 0.25) #linear interpretation 
+		rotation.y = yaw
