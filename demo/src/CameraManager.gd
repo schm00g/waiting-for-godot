65b47fcda7a17d2a00fa3bf83b33655a8e9a36b6
schm00g <95095322+schm00g@users.noreply.github.com>
2026-09-17 20:57:37 +0100

    mouse mode visible

--- demo/src/CameraManager.gd
+++ demo/src/CameraManager.gd
@@ -12,7 +12,7 @@ const CAMERA_RATIO: float = .625
 
 
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 
 func _input(p_event: InputEvent) -> void:
